c234a351f6d99fea3b16a0c908791662fe8c7784
Anox-Leo <102829993+Anox-Leo@users.noreply.github.com>
2023-05-26 12:35:54 +0200

    chore: change api key usage

--- index.js
+++ index.js
@@ -12,34 +12,8 @@ exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patch
   await log.info(`Mise à jour forcée : ${processingConfig.forceUpdate}`)
 
   await fs.ensureDirSync(path.join(tmpDir))
-  if (!fs.existsSync(path.join(tmpDir, 'token.json'))) {
-    await fs.ensureFileSync(path.join(tmpDir, 'token.json'))
-    await fs.writeFileSync(path.join(tmpDir, 'token.json'), JSON.stringify({}))
-  }
-  await log.info('Récupération du token')
-  const file = await fs.readFileSync(path.join(tmpDir, 'token.json'))
-  let token = await JSON.parse(file)
-  if (!token.access_token || token.expiration < Date.now()) {
-    token = { data: {} }
-    const key = Buffer.from(processingConfig.apiKey).toString('base64')
-    await log.info('Token expiré ou inexistant, récupération d\'un nouveau token')
-    while (!token.data.access_token) {
-      token = await axiosBis({
-        method: 'post',
-        url: 'https://api.insee.fr/token',
-        data: 'grant_type=client_credentials',
-        headers: { Authorization: 'Basic ' + key }
-      }).catch((err) => {
-        log.info('Erreur lors de la récupération du token, réessai dans 2 secondes')
-        log.info(JSON.stringify(err, null, 2))
-      })
-      await wait(2000)
-    }
-    token.data.expiration = Date.now() + token.data.expires_in * 1000
-    await fs.writeFileSync(path.join(tmpDir, 'token.json'), JSON.stringify(token.data, null, 2))
-  }
-  await download(processingConfig, tmpDir, axios, log, token.data)
-  await process(processingConfig, tmpDir, axios, log, token.data)
+  await download(processingConfig, tmpDir, axios, log, processingConfig.apiKey)
+  await process(processingConfig, tmpDir, axios, log, processingConfig.apiKey)
   if (processingConfig.datasetMode === 'update' && !processingConfig.forceUpdate) {
     try {
       await log.step('Vérification de l\'en-tête du jeu de données')
